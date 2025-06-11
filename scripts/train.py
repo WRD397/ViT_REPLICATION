a43abaa5749e9a9cfa49ce8f6a8b1fd1645c4778
WRD397 <writd97@gmail.com>
2025-06-11 10:14:03 +0530

    LR Schedulers are added.

--- scripts/train.py
+++ scripts/train.py
@@ -22,29 +22,48 @@ from pynvml import (
     nvmlDeviceGetUtilizationRates
 )
 from torch.optim.lr_scheduler import CosineAnnealingLR
+from timm.data import Mixup
+import numpy as np
+from transformers import get_cosine_schedule_with_warmup
 
-
-def train_one_epoch(model, loader, criterion, optimizer, device):
+def train_one_epoch(model, loader, criterion, optimizer, device, 
+                    mixup_fn=None, scheduler_warmup_enabled=False, scheduler_warmup=None):
     model.train()
     running_loss = 0.0
     correct = 0
     total = 0
 
-    progress_bar = tqdm(loader, desc="Training", leave=False)
-    for  inputs, labels in progress_bar:
-        inputs, labels = inputs.to(device), labels.to(device)
+    progress_bar = tqdm(loader, desc="Training", leave=True)
+    for  inputs, targets in progress_bar:
+        #print(f'input shape : {inputs.shape}, taget_shape : {targets.shape}, target dim : {targets.ndim}')
+        inputs, targets = inputs.to(device), targets.to(device)
+        if mixup_fn is not None:
+            inputs, targets = mixup_fn(inputs, targets)
+
+        if targets.ndim == 2:
+            targets = targets.type_as(inputs)
 
         optimizer.zero_grad()
         outputs = model(inputs)
-
-        loss = criterion(outputs, labels)
+        loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
+        if scheduler_warmup_enabled:
+            if scheduler_warmup is None : raise Exception(f'scheduler warmup is enabled, but no scheduler object has been passed in train_one_epoch function')
+            scheduler_warmup.step()
 
         running_loss += loss.item() * inputs.size(0)
-        _, predicted = outputs.max(1)
-        correct += predicted.eq(labels).sum().item()
-        total += labels.size(0)
+
+        if targets.ndim == 2:
+            # MixUp with soft labels
+            _, predicted = outputs.max(1)
+            _, true_classes = targets.max(1)  # Take argmax of soft labels as true class
+            correct += predicted.eq(true_classes).sum().item()
+            total += targets.size(0)
+        else :
+            _, predicted = outputs.max(1)
+            correct += predicted.eq(targets).sum().item()
+            total += targets.size(0)
 
         # Update progress bar with metrics
         if total > 0:
@@ -55,9 +74,8 @@ def train_one_epoch(model, loader, criterion, optimizer, device):
                 "Acc": f"{accuracy:.2f}%"
             })
 
+        else : raise Exception(f'Expected non-zero batch size, but got 0 targets. Check if the dataset is empty or DataLoader is misconfigured.')
 
-    avg_loss = running_loss / total
-    accuracy = 100. * correct / total
     
     return avg_loss, accuracy
 
@@ -66,18 +84,32 @@ def validate(model, loader, criterion, device):
     running_loss = 0.0
     correct = 0
     total = 0
-    progress_bar = tqdm(loader, desc="Validation", leave=False)
-
+    progress_bar = tqdm(loader, desc="Validation", leave=True)
     with torch.no_grad():
         for inputs, labels in progress_bar:
             inputs, labels = inputs.to(device), labels.to(device)
+            
+            # If labels are soft (e.g., using BCEWithLogitsLoss), convert to float
+            if labels.ndim == 2:
+                labels = labels.type_as(inputs)
 
             outputs = model(inputs)
             loss = criterion(outputs, labels)
 
             running_loss += loss.item() * inputs.size(0)
+            # Compute accuracy
             _, predicted = outputs.max(1)
-            correct += predicted.eq(labels).sum().item()
+
+            if labels.ndim == 2:
+                # Soft labels → convert to class index
+                _, true_classes = labels.max(1)
+                correct += predicted.eq(true_classes).sum().item()
+
+            else:
+                # Hard labels
+                correct += predicted.eq(labels).sum().item()
+
+
             total += labels.size(0)
 
             # Avoid division by zero on first step
@@ -89,23 +121,16 @@ def validate(model, loader, criterion, device):
                     "Loss": f"{avg_loss:.4f}",
                     "Acc": f"{accuracy:.2f}%"
                 })
-
-    avg_loss = running_loss / total
-    accuracy = 100. * correct / total
-
+                
     return avg_loss, accuracy
 
-
 def main():
-    # Load config
-    config = load_config(f"{ROOT_DIR_PATH}/config/vit_config.yaml")
-
     # Device setup
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
-    # Data loaders
-    print('loading training testing data')
+    # Load config
+    config = load_config(f"{ROOT_DIR_PATH}/config/vit_config.yaml")
     # loading cifar100
     cifar100_config = config["data"]['CIFAR100']
     DATASET = cifar100_config["dataset"]
@@ -113,6 +138,50 @@ def main():
     BATCH = cifar100_config["batch_size"]
     NUM_WORKERS = cifar100_config["num_workers"]
     IMAGE = cifar100_config["img_size"]
+    # Model
+    modelConfig = config["model"]
+    vitSmall_config = modelConfig['VIT_SMALL']
+    MODEL_NAME = vitSmall_config["name"]
+    NUM_CLASSES = vitSmall_config["num_classes"]
+    # training config
+    trainingConfig = config['training']
+    LEARNING_RATE = trainingConfig['lr']
+    EPOCHS = trainingConfig['epochs']
+    WEIGHT_DECAY = trainingConfig['weight_decay']
+    USE_SCHEDULER = trainingConfig['scheduler']
+    USE_SCHEDULER_WARMUP = trainingConfig['scheduler_warmup']
+    WARMUP_STEPS = trainingConfig['warmup_steps']
+    USE_LABEL_SMOOTHENING = trainingConfig["label_smoothing_enabled"]
+    LABEL_SMOOTHENING = trainingConfig["label_smoothing"]
+    # mixup config
+    mixupConfig = trainingConfig['mixup']
+    MIXUP_ALPHA = mixupConfig["mixup_alpha"]
+    CUTMIX_ALPHA = mixupConfig["cutmix_alpha"]
+    LABEL_SMOOTHENING_MIXUP = mixupConfig["label_smoothing_mixup"]
+    USE_MIXUP = mixupConfig["enabled"]
+   
+    # logging switches
+    print('-----------')
+    print('Label Smoothening is Enabled') if USE_LABEL_SMOOTHENING else print('Label Smoothening is Disabled')
+    print('LR Scheduler is Enabled') if USE_SCHEDULER else print('LR Scheduler is Disabled')
+    print('LR SchedulerWarmup is Enabled') if USE_SCHEDULER_WARMUP else print('LR SchedulerWarmup is Disabled')
+    print('MixUp is Enabled') if USE_MIXUP else print('MixUp is Disabled')
+    print('-----------')
+    # === Mixup Setup ===
+    mixup_fn = None
+    if USE_MIXUP:
+        mixup_fn = Mixup(
+            mixup_alpha=MIXUP_ALPHA,
+            cutmix_alpha=CUTMIX_ALPHA,
+            label_smoothing=LABEL_SMOOTHENING_MIXUP,
+            num_classes=NUM_CLASSES
+        )
+        train_criterion = nn.BCEWithLogitsLoss()
+        val_criterion = nn.CrossEntropyLoss()
+    else:        
+        mixup_fn = None
+        train_criterion = nn.CrossEntropyLoss(label_smoothing=LABEL_SMOOTHENING if USE_LABEL_SMOOTHENING else 0.0)
+        val_criterion = train_criterion
 
     # loading data
     print(f'loading dataset : {DATASET}')
@@ -127,32 +196,40 @@ def main():
     for images, labels in train_loader:
         print(f'image shape and labels shape in training data - one batch : {images.shape}, {labels.shape}')
         break
-
-    # Model
-    modelConfig = config["model"]
-    vitSmall_config = modelConfig['VIT_SMALL']
-    MODEL_NAME = vitSmall_config["name"]
-    LEARNING_RATE = vitSmall_config['lr']
-    EPOCHS = vitSmall_config['epochs']
-
-    model = VisionTransformerSmall(config).to(device)
     
-    # printing model config
+    # loading model
+    model = VisionTransformerSmall(config).to(device)
+    # logging model parameters and config
     print("Model Configuration:")
     for k, v in vitSmall_config.items():
         print(f"  {k}: {v}")
-    # Count and print model parameters
     total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f"Total Trainable Parameters: {total_params:,} ({total_params / 1e6:.2f}M)")
+    print(f'printing a few of the model weights - should be random and unique in every run.')
+    print(model.patch_embed.projection.weight[0][0][:5]) 
 
-    # Loss and optimizer
-    criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    scheduler = CosineAnnealingLR(optimizer, T_max=EPOCHS)
+    # optimizer & scheduler
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+    scheduler_warmup_obj = None
+    scheduler_warmup_enabled_flag = False
+    if USE_SCHEDULER : 
+        scheduler = CosineAnnealingLR(optimizer, T_max=EPOCHS)
+    elif USE_SCHEDULER_WARMUP:
+        scheduler_warmup_enabled_flag = True
+        num_training_steps = EPOCHS * len(train_loader)
+        num_warmup_steps = WARMUP_STEPS * len(train_loader)
 
+        scheduler_warmup = get_cosine_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=num_warmup_steps,
+            num_training_steps=num_training_steps
+        )
+        scheduler_warmup_obj = scheduler_warmup
+
+    # monitors initialization
     best_val_acc = 0.0
+    best_val_loss = np.inf
     best_model_state = None
-    
     # gpu utilization
     max_mem_used = 0
     max_gpu_util = 0
@@ -167,10 +244,13 @@ def main():
     for epoch in range(EPOCHS):
         print(f"\nEpoch {epoch+1}/{EPOCHS}")
 
-        train_loss, train_acc = train_one_epoch(model, train_loader, criterion, optimizer, device)
-        val_loss, val_acc = validate(model, val_loader, criterion, device)
-        scheduler.step()
-        
+        train_loss, train_acc = train_one_epoch(model, train_loader, train_criterion, optimizer, device, 
+                                                mixup_fn=mixup_fn,
+                                                scheduler_warmup_enabled=scheduler_warmup_enabled_flag,
+                                                scheduler_warmup=scheduler_warmup_obj)
+        val_loss, val_acc = validate(model, val_loader, val_criterion, device)
+        if USE_SCHEDULER : scheduler.step()
+
         # Monitor GPU usage
         mem_info = nvmlDeviceGetMemoryInfo(handle)
         util_info = nvmlDeviceGetUtilizationRates(handle)
@@ -185,16 +265,21 @@ def main():
             corresponding_train_acc = train_acc
             corresponding_train_loss = train_loss
             best_model_state = model.state_dict()
-    
+        
+        endTime = time.time()
+        elapsedTime = endTime - startTime
+        hours = int(elapsedTime // 3600)
+        minutes = int((elapsedTime % 3600) // 60)
+        seconds = int(elapsedTime % 60)
+        print(f"Elapsed Time : {hours}h : {minutes}m : {seconds}s")
   
-    endTime = time.time()
-    elapsedTime = endTime - startTime
-    # Convert seconds to h:m:s
-    hours = int(elapsedTime // 3600)
-    minutes = int((elapsedTime % 3600) // 60)
-    seconds = int(elapsedTime % 60)
+    # endTime = time.time()
+    # elapsedTime = endTime - startTime
+    # hours = int(elapsedTime // 3600)
+    # minutes = int((elapsedTime % 3600) // 60)
+    # seconds = int(elapsedTime % 60)
     
-    # Shutdown 
+    # gpu monitoring shutdown 
     nvmlShutdown()
     
     print('\n====== Model Performance =======')
@@ -204,13 +289,15 @@ def main():
     print(f"GPU Used : {gpu_name}")
     print(f"Peak GPU Memory: {max_mem_used:.2f} MB")
     print(f"Peak GPU Utilization: {max_gpu_util}%")
-    print(f"Peak Memory Bandwidth Utilization: {max_mem_util}%\n")
+    print(f"Peak Memory Bandwidth Utilization: {max_mem_util}%")
     print(f"\nTraining completed in: {hours}h : {minutes}m : {seconds}s\n\n")
 
     # # Save model
     # save_model(model_state=best_model_state, model_name=modelName, epoch=epoch, val_acc=best_val_acc)
     # print(f"Model saved as {modelName} inside checkpoints")
-
+    
 
 if __name__ == "__main__":
+    torch.cuda.empty_cache()
     main()
+    
